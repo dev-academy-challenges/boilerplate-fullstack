cc34165786e06ba7617e1c7bcbf1542598be97a7
Joshua Vial <joshua@enspiral.com>
2019-09-19 13:08:29 +1200

    removing unnecessary config

--- tests/client/App.test.js
+++ tests/client/App.test.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Enzyme, {shallow, render, mount} from 'enzyme'
+import {shallow, render, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import './config/enzyme'
 
 import App from '../../client/components/App'
 App.prototype.componentDidMount = () => {}
-
-Enzyme.configure({adapter: new Adapter()})
 
 test('test runner is working', () => {
   expect(true).toBeTruthy()
