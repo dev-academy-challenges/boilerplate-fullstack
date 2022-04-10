d4ca47291d950f6ab59be72fd2f7b9490c1c09e6
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-04-10 20:44:53 +1200

    Make ReactDOM.render stuff consistent with other challenges

--- client/index.js
+++ client/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(
+  ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>,
