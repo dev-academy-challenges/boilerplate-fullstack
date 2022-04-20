ce020c9a6e12d0da0717178d10040bf8497576ce
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-04-21 00:31:34 +1200

    Use payload and destructure the action

--- client/actions/index.js
+++ client/actions/index.js
@@ -2,19 +2,18 @@ import { getFruits } from '../apis/fruits'
 
 export const SET_FRUITS = 'SET_FRUITS'
 
-export function setFruits (fruits) {
+export function setFruits(fruits) {
   return {
     type: SET_FRUITS,
-    fruits
+    payload: fruits,
   }
 }
 
-export function fetchFruits () {
-  return dispatch => {
-    return getFruits()
-      .then(fruits => {
-        dispatch(setFruits(fruits))
-        return null
-      })
+export function fetchFruits() {
+  return (dispatch) => {
+    return getFruits().then((fruits) => {
+      dispatch(setFruits(fruits))
+      return null
+    })
   }
 }
